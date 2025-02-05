b41c40e4e7cb564ff0c1274bb08052341ea6380c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-05 13:53:20 +0000

    refactor: switch to using express

--- src/index.js
+++ src/index.js
@@ -20,3 +20,38 @@
 //   console.log('Server connected on: http://localhost:8080');
 // });
 
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const handleError = (err, res) => {
+  console.error('Error: ', err);
+  if (err.code === 'ENOENT') {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+  } else {
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+
+  res.sendFile(filePath, (err) => {
+    if (err) handleError(err, res);
+  });
+});
+
+app.get('/:name', (req, res) => {
+  const filePath = path.join(__dirname, `${req.params.name}.html`);
+  
+  res.sendFile(filePath, (err) => {
+    if (err) handleError(err, res);
+  });
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server connected on: http://localhost:${PORT}`);
+});
