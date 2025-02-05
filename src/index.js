3bf00159a89aab30f333bd096b6194fb6203ce10
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-05 13:11:55 +0000

    feat: comment out previous solution using http and fs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-const http = require('http');
-const fs = require('fs').promises;
+// const http = require('http');
+// const fs = require('fs').promises;
 
-const server = http.createServer(async (req, res) => {
-  try {
-    let filePath = req.url === '/' ? './index.html': `.${req.url}.html`;
+// const server = http.createServer(async (req, res) => {
+//   try {
+//     let filePath = req.url === '/' ? './index.html': `.${req.url}.html`;
 
-    const data = await fs.readFile(filePath, 'utf-8');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  } catch (err) {
-    const notFound = await fs.readFile('404.html', 'utf-8');
-    console.error('Error: ', err);
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end(notFound);
-  }
-});
+//     const data = await fs.readFile(filePath, 'utf-8');
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end(data);
+//   } catch (err) {
+//     const notFound = await fs.readFile('404.html', 'utf-8');
+//     console.error('Error: ', err);
+//     res.writeHead(404, { 'Content-Type': 'text/html' });
+//     res.end(notFound);
+//   }
+// });
 
-server.listen(8080, () => {
-  console.log('Server connected on: http://localhost:8080');
-});
+// server.listen(8080, () => {
+//   console.log('Server connected on: http://localhost:8080');
+// });
+
